01930e6b6bc42001eeab758a8088a223d8ede35d
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-07-22 19:40:19 +0300

    додано logout і current

---
+++ controllers/users/current.js
@@ -0,0 +1,11 @@
+const { ctrlWrapper } = require("../../helpers");
+
+const current = async (req, res) => {
+  const { email, subscription } = req.user;
+  res.status(200).json({
+    email,
+    subscription,
+  });
+};
+
+module.exports = ctrlWrapper(current);
