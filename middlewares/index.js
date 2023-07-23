48aec92bdda8608c3304fb945f6616712a2c526a
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-07-23 17:45:56 +0300

    непрацюючий routes/auth.js (помилка)

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,3 +1,7 @@
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
-module.exports = authenticate;
+module.exports = {
+  authenticate,
+  upload,
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const ctrl = require("../../controllers");
-const authenticate = require("../../middlewares");
+const { authenticate, upload } = require("../../middlewares");
 const router = express.Router();
+
+console.log(upload);
 
 router.post("/users/register", ctrl.users.register);
 router.post("/users/login", ctrl.users.login);
@@ -11,6 +13,12 @@ router.patch(
   "/users/subscription",
   authenticate,
   ctrl.users.updateSubscription
+);
+router.patch(
+  "/users/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrl.users.updateAvatar
 );
 
 module.exports = router;
