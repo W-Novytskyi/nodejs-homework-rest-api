b96beadd2a6414cc3d68e3a8406b835836d688ef
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-07-28 16:17:44 +0300

    додано Dockerfile

--- server.js
+++ server.js
@@ -16,8 +16,3 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-// NFpDcwguFvPZGCph    NFpDcwguFvPZGCph
-
-// , () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
